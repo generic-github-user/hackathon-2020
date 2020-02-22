53a4ed2a49521855949630a15a60c4bb82aaeb90
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 10:51:03 -0500

    Registration page JavaScript

---
+++ js/register.js
@@ -0,0 +1,18 @@
+// Register new account on button press
+$('button#register').click(function() {
+      // Collect account information
+      var email = $('input#email').val();
+      var password = $('input#password').val();
+      // Create account
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function(result) {
+                  window.location.replace('./index.html');
+            }).catch(function(error) {
+                  console.error(error);
+            });
+
+      // Reset input fields
+      $('input#email').val('');
+      $('input#password').val('');
+      $('input#confirm-password').val('');
+});
