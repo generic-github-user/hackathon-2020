15684dfa178656568d015f7e9151410db23454b3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 17:31:32 -0500

    Student data

---
+++ js/students.js
@@ -0,0 +1,10 @@
+firebase.firestore().collection('users').where('type', '==', 'Student').get().then(function(student_data) {
+      student_data.forEach(function(student) {
+            var info = student.data();
+            var table_row = $('<tr>');
+            table_row.append($('<td class="mdl-data-table__cell--non-numeric">' + info.name + '</td>'));
+            table_row.append($('<td>' + info.grade + '</td>'));
+            table_row.append($('<td>' + info.concern + '</td>'));
+            $('#students-table-body').append(table_row);
+      })
+});
