eb0468483365921fc95f332a31ff3bdaae9610eb
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 10:48:02 -0500

    Add Firebase config file

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCB50dCZCQImUMLBaZfKH65k81V72zV5Vw",
+    authDomain: "hackathon-2020-7f75f.firebaseapp.com",
+    databaseURL: "https://hackathon-2020-7f75f.firebaseio.com",
+    projectId: "hackathon-2020-7f75f",
+    storageBucket: "hackathon-2020-7f75f.appspot.com",
+    messagingSenderId: "1064903517783",
+    appId: "1:1064903517783:web:1473da587176b98d342512",
+    measurementId: "G-5TCN9LZGB0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
