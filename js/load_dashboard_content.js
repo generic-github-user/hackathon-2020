890396387ad3ed98c142c8feecb2365f557dd2c3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 17:04:43 -0500

    Beautify JavaScript

--- js/load_dashboard_content.js
+++ js/load_dashboard_content.js
@@ -1,10 +1,10 @@
 tab = new URL(window.location.href).searchParams.get('tab');
 if (tab == undefined) {
-  tab = 'main';
+      tab = 'main';
 }
 console.log('Loading tab: ' + tab);
 $('#main-content').load('/' + tab + '.html');
 
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '/css/' + tab + '.css') );
+$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', '/css/' + tab + '.css'));
 $.getScript('/js/' + tab + '.js');
 $.getScript('https://code.getmdl.io/1.3.0/material.min.js');
--- js/report.js
+++ js/report.js
@@ -1,14 +1,14 @@
 $('#submit-report-button').click(() => {
-  firebase.firestore().collection("reports").add({
-      type: $('#report-type-dropdown').val(),
-      content: $('#report-textarea').val()
-  })
-  .then(function(docRef) {
-      console.log("Document written with ID: ", "REDACTED");
-      window.location.href = '?tab=main';
-      snackbar('Report submitted.');
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });
+      firebase.firestore().collection("reports").add({
+                  type: $('#report-type-dropdown').val(),
+                  content: $('#report-textarea').val()
+            })
+            .then(function(docRef) {
+                  console.log("Document written with ID: ", "REDACTED");
+                  window.location.href = '?tab=main';
+                  snackbar('Report submitted.');
+            })
+            .catch(function(error) {
+                  console.error("Error adding document: ", error);
+            });
 });
