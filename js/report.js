ac8b3c928271ff80a4fedb5c75043694172e4d22
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 15:39:01 -0500

    Redirect to main tab after submitting report

--- js/report.js
+++ js/report.js
@@ -5,6 +5,7 @@ $('#submit-report-button').click(() => {
   })
   .then(function(docRef) {
       console.log("Document written with ID: ", "REDACTED");
+      window.location.href = '?tab=main';
   })
   .catch(function(error) {
       console.error("Error adding document: ", error);
