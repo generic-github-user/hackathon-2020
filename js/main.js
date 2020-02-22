640e0145c863f7b1df5f0d7e2100c496ffd72fd0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 16:23:19 -0500

    Store vibes.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('#submit-feeling-button').click(() => {
+  var vibe = parseInt($('#mood-slider').val());
+  $('#vibe-container').css('visibility', 'hidden');
+
+  firebase.firestore().collection("vibes").add({
+      student: firebase.auth().currentUser.uid,
+      time: new Date().getTime(),
+      level: vibe
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      if (vibe >= 50) {
+        snackbar('Glad you feel well today!');
+      } else {
+        snackbar('Hope you feel better soon!');
+      }
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+})
