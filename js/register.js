c8fd0f8c2a5443cd563a2add436a4ae3272dbfe6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 16:37:40 -0500

    Store registration information in database

--- js/register.js
+++ js/register.js
@@ -1,12 +1,25 @@
 // Register new account on button press
 $('button#register').click(function() {
       // Collect account information
-      var email = $('input#email').val();
+      var email_address = $('input#email').val();
       var password = $('input#password').val();
       // Create account
-      firebase.auth().createUserWithEmailAndPassword(email, password)
+      firebase.auth().createUserWithEmailAndPassword(email_address, password)
             .then(function(result) {
-                  window.location.replace('./index.html');
+                  var userId = firebase.auth().currentUser.uid
+                  var user_type = $('input#user-type').val()
+                  if (user_type == undefined) {
+                        user_type = 'Student';
+                  }
+                  firebase.firestore().collection('users').doc(userId).set({
+                        name: $('input#name').val(),
+                        email: email_address,
+                        zip: $('input#zip').val(),
+                        school: $('input#school').val(),
+                        type: user_type
+                  }).then(() => {
+                        window.location.replace('./dashboard.html');
+                  });
             }).catch(function(error) {
                   console.error(error);
             });
