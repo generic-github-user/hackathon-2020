d5921c72256aa01f57fce700578eeb2d25d3ece3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 14:26:24 -0500

    Load scripts and styles after loading HTML content

--- js/load_dashboard_content.js
+++ js/load_dashboard_content.js
@@ -4,3 +4,7 @@ if (tab == undefined) {
 }
 console.log('Loading tab: ' + tab);
 $('#main-content').load('/' + tab + '.html');
+
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '/css/' + tab + '.css') );
+$.getScript('/js/' + tab + '.js');
+$.getScript('https://code.getmdl.io/1.3.0/material.min.js');
