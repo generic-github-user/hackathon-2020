b3f7cd6b2092b4916ebbfaf33c7cee7e2fbbf3f4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 22:40:39 -0500

    Update event firing order

--- js/data.js
+++ js/data.js
@@ -1,58 +1,57 @@
 var color = Chart.helpers.color;
 
-function generateData() {
+window.onload = function() {
       var data = [];
       firebase.firestore().collection('users').where('type', '==', 'student').get().then(
             function(students) {
                   students.forEach(
                         function(student) {
-                              // student.ref.collection('vibes').get().then(
-                              //       function(vibes) {
-                              //             var vibe_average = 0;
-                              //
-                              //             vibes.forEach(
-                              //                   function(vibe) {
-                              //                         vibe_average += vibe.data().level;
-                              //                   }
-                              //             );
-                              //
-                              //             vibe_average /= 10;
-                              //
-                              //       }
-                              // )
-                              console.log(student.data().average_score)
-                              info = student.data();
-                              data.push({
-                                    x: info.average_score,
-                                    y: 5
-                              });
+                              student.ref.collection('vibes').get().then(
+                                    function(vibes) {
+                                          var vibe_average = 0;
+
+                                          vibes.forEach(
+                                                function(vibe) {
+                                                      vibe_average += vibe.data().level;
+                                                }
+                                          );
+
+                                          vibe_average /= 10;
+
+                                          console.log(student.data().average_score)
+                                          info = student.data();
+                                          data.push({
+                                                x: info.average_score,
+                                                y: vibe_average
+                                          });
+
+                                          var scatterChartData = {
+                                                datasets: [{
+                                                      label: 'My First dataset',
+                                                      borderColor: window.chartColors.red,
+                                                      backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
+                                                      data: data
+                                                }]
+                                          };
+
+                                          var ctx = document.getElementById('canvas').getContext('2d');
+                                          window.myScatter = Chart.Scatter(ctx, {
+                                                data: scatterChartData,
+                                                options: {
+                                                      title: {
+                                                            display: true,
+                                                            text: 'Chart.js Scatter Chart'
+                                                      },
+                                                }
+                                          });
+
+                                          window.myScatter.update();
+                                    }
+                              )
                         }
                   )
+
+
             }
       )
-      return data;
-}
-
-window.onload = function() {
-      var scatterChartData = {
-            datasets: [{
-                  label: 'My First dataset',
-                  borderColor: window.chartColors.red,
-                  backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
-                  data: generateData()
-            }]
-      };
-
-      var ctx = document.getElementById('canvas').getContext('2d');
-      window.myScatter = Chart.Scatter(ctx, {
-            data: scatterChartData,
-            options: {
-                  title: {
-                        display: true,
-                        text: 'Chart.js Scatter Chart'
-                  },
-            }
-      });
-
-      window.myScatter.update();
 };
