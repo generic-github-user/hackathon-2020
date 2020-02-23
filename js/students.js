17e8c1867ed4aea57b10f5ced73f8534c153cffe
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-22 20:03:39 -0500

    calculate concern score

--- js/students.js
+++ js/students.js
@@ -4,7 +4,9 @@ firebase.firestore().collection('users').where('type', '==', 'student').get().th
             var table_row = $('<tr>');
             table_row.append($('<td class="mdl-data-table__cell--non-numeric">' + info.name + '</td>'));
             table_row.append($('<td>' + info.grade + '</td>'));
-            table_row.append($('<td>' + info.concern + '</td>'));
+            table_row.append($('<td>' + info.average_score + '</td>'));
+            var concern = Math.round(100 - info.average_score);
+            table_row.append($('<td>' + concern + '</td>'));
             $('#students-table-body').append(table_row);
       })
 });
