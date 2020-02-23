b2e8eb9d7c9195673b90f2994e460034e0369265
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-23 00:06:30 -0500

    Use Promise.all() to wait until all data is loaded to generate graph

--- js/data.js
+++ js/data.js
@@ -1,71 +1,91 @@
 var color = Chart.helpers.color;
 
+function generateGraph() {
+      scatterChartData = {
+            datasets: [{
+                  label: 'Students',
+                  borderColor: window.chartColors.red,
+                  backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
+                  data: data
+            }]
+      };
+
+      var ctx = document.getElementById('canvas').getContext('2d');
+      window.myScatter = Chart.Scatter(ctx, {
+            data: scatterChartData,
+            options: {
+                  title: {
+                        display: true,
+                        text: 'Chart.js Scatter Chart'
+                  },
+                  scales: {
+                        xAxes: [{
+                              scaleLabel: {
+                                    display: true,
+                                    labelString: 'Average Grade'
+                              }
+                        }],
+                        yAxes: [{
+                              scaleLabel: {
+                                    display: true,
+                                    labelString: 'Positivity'
+                              }
+                        }]
+                  }
+            }
+      });
+
+      window.myScatter.update();
+}
+
 window.onload = function() {
-      var data = [];
+      data = [];
       firebase.firestore().collection('users').where('type', '==', 'student').get().then(
-            function(students) {
+            (students) => {
+                  var results = []
                   students.forEach(
-                        function(student) {
-                              student.ref.collection('vibes').get().then(
-                                    function(vibes) {
-                                          var vibe_average = 0;
-
-                                          vibes.forEach(
-                                                function(vibe) {
-                                                      vibe_average += vibe.data().level;
-                                                }
-                                          );
-
-                                          vibe_average /= 10;
-
-                                          console.log(student.data().average_score)
-                                          info = student.data();
-                                          data.push({
-                                                x: info.average_score,
-                                                y: vibe_average
-                                          });
-
-                                          var scatterChartData = {
-                                                datasets: [{
-                                                      label: 'Students',
-                                                      borderColor: window.chartColors.red,
-                                                      backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
-                                                      data: data
-                                                }]
-                                          };
-
-                                          var ctx = document.getElementById('canvas').getContext('2d');
-                                          window.myScatter = Chart.Scatter(ctx, {
-                                                data: scatterChartData,
-                                                options: {
-                                                      title: {
-                                                            display: true,
-                                                            text: 'Chart.js Scatter Chart'
-                                                      },
-                                                      scales: {
-                                                            xAxes: [{
-                                                                  scaleLabel: {
-                                                                        display: true,
-                                                                        labelString: 'Average Grade'
-                                                                  }
-                                                            }],
-                                                            yAxes: [{
-                                                                  scaleLabel: {
-                                                                        display: true,
-                                                                        labelString: 'Positivity'
-                                                                  }
-                                                            }]
-                                                      }
-                                                }
-                                          });
-
-                                          window.myScatter.update();
-                                    }
-                              )
-                        }
+                        student => results.push(student)
                   )
 
+                  Promise.all(results).then(
+                        (results) => {
+                              results.forEach(
+                                    (student) => {
+                                          // student.ref.collection('vibes').get().then(
+                                          //       function(vibes) {
+                                          //             var vibe_average = 0;
+                                          //
+                                          //             vibes.forEach(
+                                          //                   function(vibe) {
+                                          //                         vibe_average += vibe.data().level;
+                                          //                   }
+                                          //             );
+                                          //
+                                          //             vibe_average /= 10;
+                                          //
+                                          //             console.log(student.data().average_score)
+                                          //             info = student.data();
+                                          //             data.push({
+                                          //                   x: info.average_score | 5,
+                                          //                   y: vibe_average | 5
+                                          //             });
+                                          //       }
+                                          // )
 
+                                          student_data = student.data();
+                                          var concern = Math.round(100 - student_data.average_score);
+                                          data.push({
+                                                x: student_data.average_score | 5,
+                                                y: concern | 5
+                                          });
+                                    }
+                              );
+
+                              generateGraph();
+                        }
+                  )
             }
       )
 };
+
+// setTimeout(generateGraph, 3000)
