e852db2de6669527693a7955d0604ee6a6dd5be5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-02-23 10:11:48 -0500

    Hide some panels for student users

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,21 @@
 function auth_change() {
       if (firebase.auth().currentUser == null) {
             window.location.href = './login.html';
+      } else {
+            firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get().then(function(doc) {
+                  if (doc.exists) {
+                        if (doc.data().type == 'student' || doc.data().type == 'Student') {
+                              $('a[href$="alerts"]').remove();
+                              $('a[href$="students"]').remove();
+                              $('a[href$="data"]').remove();
+                        }
+                  } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
+                  }
+            }).catch(function(error) {
+                  console.log("Error getting document:", error);
+            });
       }
 
       var email = firebase.auth().currentUser.email;
